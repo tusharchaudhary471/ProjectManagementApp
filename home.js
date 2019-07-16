87968c76964a4b7ba66be1b77589dd54c88bfc9e
tushar <tusharchaudhary471@gmail.com>
2019-07-16 22:55:12 +0530

    version1

---
+++ home.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var toggleVariable = document.getElementsByClassName('menu-block')[0];
+    if(toggleVariable.style.display == 'none') {
+        toggleVariable.style.display = '';
+    }else {
+        toggleVariable.style.display = 'none'
+    }
+}
